7592b727e3170206dca003312ea0c78f6bb3975e
xrtf16 <102335749+xrtf16@users.noreply.github.com>
2022-06-18 05:34:59 +0300

    Update and rename program/music.py to برنامج/music.py

--- برنامج/music.py
+++ برنامج/music.py
@@ -39,12 +39,12 @@ async def ytdl(format: str, link: str):
     return 0, stderr
 
 
-@Client.on_message(command(["تشغيل", f"play@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["شغل","ش" f"play@{BOT_USERNAME}"]) & other_filters)
 async def play(c: Client, m: Message):
     await m.delete()
     do = requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/getChatMember?chat_id=@{UPDATES_CHANNEL}&user_id={m.from_user.id}").text
     if do.count("left") or do.count("Bad Request: user not found"):
-        await m.reply_text(f" ** ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉**\n@{UPDATES_CHANNEL}\n» **اشتࢪك بقناة البوت لتستطيع تشغيل الاغاني**")
+        await m.reply_text(f" ** ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉**\n@{UPDATES_CHANNEL}\n» **اشتࢪك بقناة البوت من اجل تشغيل او الاستماع للاغاني  .**")
     else:
         replied = m.reply_to_message
         chat_id = m.chat.id
